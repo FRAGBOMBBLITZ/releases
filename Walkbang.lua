77c46c5af68b83167c9a0d2910c1d2b7babeeffb
ches <94960650+FRAGBOMBBLITZ@users.noreply.github.com>
2025-02-08 13:18:00 +1300

    Update Walkbang.lua

--- Walkbang.lua
+++ Walkbang.lua
@@ -1,6 +1,5 @@
 --[=[
 
-
 db   d8b   db  .d8b.  db      db   dD d8888b.  .d8b.  d8b   db  d888b  
 88   I8I   88 d8' `8b 88      88 ,8P' 88  `8D d8' `8b 888o  88 88' Y8b 
 88   I8I   88 88ooo88 88      88,8P   88oooY' 88ooo88 88V8o 88 88      
@@ -12,4 +11,5 @@ To walkbang a player, hover over them and press B to toggle, or use the GUI.
 Script by Ches / @FRAGBOMBBLITZ, have fun!                                                   
 
 ]=]
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FRAGBOMBBLITZ/sources/refs/heads/main/Walkbang.lua"))()
